e490db58e3ec67e4c109d7925d1ba62ff82d1643
Ricardo Monteiro <ricardohenrique071@gmail.com>
2021-10-04 17:08:51 -0300

    insert get

--- lib/controllers/buttom_navigator_controller.dart
+++ lib/controllers/buttom_navigator_controller.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class ButtonNavigatorController extends ChangeNotifier {
   int currentIndex = 0;
+  
+  get int getCurrentIndex ()=> currentIndex;
 
   void setCurrentIndex(int value) {
     currentIndex = value;
